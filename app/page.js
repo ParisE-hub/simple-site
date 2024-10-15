b88f4295c8b77bbf7a693383dc0b521ed9a4168b
ParisE-hub <apraze2006@gmail.com>
2024-10-15 10:59:07 +0100

    added a text banner

--- app/page.js
+++ app/page.js
@@ -4,6 +4,10 @@ export default function Page() {
      return (
         <main>
             <Header/>
+            <div className="text">
+            <h1>Title Description</h1>
+             <p>Title Description</p>
+             </div>
             <div className="banner">
             <h1>My Home Page</h1>
             </div>
